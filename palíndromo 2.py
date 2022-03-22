b4104b4a4f3a3ee6493d611f71b803d10b9c5e89
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-03-22 13:10:16 +0100

    Ejercicio 2, creamos la clase palíndromo y la funcion que verifique

--- palíndromo 2.py
+++ palíndromo 2.py
@@ -3,25 +3,17 @@
 #Además, al destruir la instancia, muestre el atributo en mayúsculas.
 
 
-class Cadenas:
+class Palindromo:
     def __init__(self, cadena1):
         self.cadena1=cadena1
     
-    def Palindromo(self):
-        cadena1=self.cadena1
-        c,i,nom,cad,x = 0,0,'','',''
-        i = len(cadena1)
-        nom = cadena1.lower()
-        while i != c:
-            for x in nom:
-                cad = x + cad
-                c = c+1
-            if nom == cad:
+    def Verificacion(self):
+        cadena1= input("Dame una palabra: ")
+        print(cadena1.upper())
+            if :
                 return str("True")
             else:
                 return str("False")
 
-cadena1 = input("Dame una palabra: ")
-op1 = Cadenas(cadena1)
 
-print(op1.Palindromo())
+print(Palindromo.Verificacion())
