e7aa999569cf55c6d7d377d6ce761e6be74c167a
lauralardies <lrodrlar@myuax.com>
2022-03-22 08:49:28 +0100

    comentamos el archivo logger

--- logger.py
+++ logger.py
@@ -1,11 +1,11 @@
 class Test:
-    def __init__(self) -> None:
+    def __init__(self) -> None: # Inicializamos un contador y avisamos que comienza a contar, imprimiendo en mensaje "Start log"
         self.contador = 0
         print("--Start log--")
 
-    def __del__(self):
+    def __del__(self): # Al final del código esta función se ejecuta automáticamente y avisa que deja de contar las llamadas mostrando el mensaje "End log". Además indica cuántas llamadas se han contado.
         print("--End log: " + str(self.contador) + " log(s)--")
     
-    def llamada(self, mensaje):
+    def llamada(self, mensaje): # Esta función aumenta el contador cada vez que entra una llamada.
         self.contador = self.contador + 1
         print(mensaje)
